ae702693fb20e41cb06cb566ba24ec0a78e8bab8
adarshsrajesh <adarshsrajesh@gmail.com>
2025-05-16 11:37:13 +0530

    fix

--- script.js
+++ script.js
@@ -17,8 +17,30 @@ const iceServers = {
     { urls: 'stun:stun1.l.google.com:19302' },
     { urls: 'stun:stun2.l.google.com:19302' },
     { urls: 'stun:stun3.l.google.com:19302' },
-    { urls: 'stun:stun4.l.google.com:19302' }
-  ]
+    { urls: 'stun:stun4.l.google.com:19302' },
+    // Add TURN servers as fallback
+    {
+      urls: [
+        'turn:openrelay.metered.ca:80',
+        'turn:openrelay.metered.ca:443',
+        'turn:openrelay.metered.ca:443?transport=tcp'
+      ],
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: [
+        'turn:numb.viagenie.ca',
+        'turn:numb.viagenie.ca:3478'
+      ],
+      username: 'webrtc@live.com',
+      credential: 'muazkh'
+    }
+  ],
+  iceCandidatePoolSize: 10,
+  iceTransportPolicy: 'all',
+  bundlePolicy: 'max-bundle',
+  rtcpMuxPolicy: 'require'
 };
 
 // Connection status handling
